3f02c193b61aa66732fbfa6fc66e53e777345965
Jesse Tham <9289224+jessetham@users.noreply.github.com>
2024-12-04 21:55:03 -0800

    Finish day 5

---
+++ src/Day05.kt
@@ -0,0 +1,48 @@
+fun main() {
+    val testInput = readInputAsText("Day05_test")
+    val testParsed = parse(testInput)
+    check(part1(testParsed.first, testParsed.second) == 143)
+    check(part2(testParsed.first, testParsed.second) == 123)
+
+    val input = readInputAsText("Day05")
+    val parsed = parse(input)
+    part1(parsed.first, parsed.second).println()
+    part2(parsed.first, parsed.second).println()
+}
+
+private fun parse(input: String): Pair<Map<Int, List<Int>>, List<List<Int>>> {
+    val (orderingRules, updates) = input.split("\n\n")
+    val inedges = mutableMapOf<Int, MutableList<Int>>()
+    for (line in orderingRules.lines()) {
+        val (from, to) = line.split('|').let { it[0].toInt() to it[1].toInt() }
+        val edges = inedges.getOrElse(to) { mutableListOf() }
+        edges.add(from)
+        inedges[to] = edges
+    }
+    val parsedUpdates = updates.lines().map { line -> line.split(",").map { it.toInt() } }
+    return inedges to parsedUpdates
+}
+
+// Not the fastest topological sort algorithm, but it gets the job done
+private fun topologicalSort(inedges: Map<Int, List<Int>>, update: List<Int>): List<Int> {
+    val numsInUpdate = update.toSet()
+    val seen = mutableSetOf<Int>()
+    val reordered = mutableListOf<Int>()
+    while (reordered.size < update.size) {
+        val next = update.first { u ->
+            val edges = inedges.getOrElse(u) { mutableListOf() }
+            edges.all { it !in numsInUpdate || it in seen } && u !in seen
+        }
+        reordered.add(next)
+        seen.add(next)
+    }
+    return reordered
+}
+
+private fun part1(inedges: Map<Int, List<Int>>, updates: List<List<Int>>) =
+    updates.filter { topologicalSort(inedges, it) == it }.sumOf { it[it.size / 2] }
+
+private fun part2(inedges: Map<Int, List<Int>>, updates: List<List<Int>>) =
+    updates.map { topologicalSort(inedges, it) }
+        .filterIndexed { i, sorted -> sorted != updates[i] }
+        .sumOf { it[it.size / 2] }
