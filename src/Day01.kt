a3d33d2f7ab64d8c7aa2a195bc1b2691986ebd10
Jesse Tham <9289224+jessetham@users.noreply.github.com>
2024-12-06 08:51:57 -0800

    Wrap functions in namespace

--- src/Day01.kt
+++ src/Day01.kt
@@ -3,31 +3,35 @@ import kotlin.math.absoluteValue
 fun main() {
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInputAsLines("Day01_test")
-    check(part1(parseInput(testInput)) == 11)
-    check(part2(parseInput(testInput)) == 31)
+    val testParsed = Day01.parse(testInput)
+    check(Day01.part1(testParsed) == 11)
+    check(Day01.part2(testParsed) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInputAsLines("Day01")
-    part1(parseInput(input)).println()
-    part2(parseInput(input)).println()
+    val parsed = Day01.parse(input)
+    Day01.part1(parsed).println()
+    Day01.part2(parsed).println()
 }
 
-private fun parseInput(input: List<String>): List<List<Int>> {
-    val nums = mutableListOf(mutableListOf<Int>(), mutableListOf())
-    for (line in input) {
-        val split = line.split("   ")
-        nums[0].add(split[0].toInt())
-        nums[1].add(split[1].toInt())
+private object Day01 {
+    fun parse(input: List<String>): List<List<Int>> {
+        val nums = mutableListOf(mutableListOf<Int>(), mutableListOf())
+        for (line in input) {
+            val split = line.split("   ")
+            nums[0].add(split[0].toInt())
+            nums[1].add(split[1].toInt())
+        }
+        return nums
     }
-    return nums
-}
 
-private fun part1(input: List<List<Int>>): Int {
-    val sorted = input.map { it.sorted() }
-    return (sorted[0] zip sorted[1]).sumOf { (it.first - it.second).absoluteValue }
-}
+    fun part1(input: List<List<Int>>): Int {
+        val sorted = input.map { it.sorted() }
+        return (sorted[0] zip sorted[1]).sumOf { (it.first - it.second).absoluteValue }
+    }
 
-private fun part2(input: List<List<Int>>): Int {
-    val counter = input[1].groupingBy { it }.eachCount()
-    return input[0].sumOf { it * counter.getOrDefault(it, 0) }
+    fun part2(input: List<List<Int>>): Int {
+        val counter = input[1].groupingBy { it }.eachCount()
+        return input[0].sumOf { it * counter.getOrDefault(it, 0) }
+    }
 }
--- src/Day04.kt
+++ src/Day04.kt
@@ -2,69 +2,75 @@ import kotlin.math.sign
 
 fun main() {
     val testInput = readInputAsLines("Day04_test")
-    val testParsed = parse(testInput)
-    check(part1(testParsed) == 18)
-    check(part2(testParsed) == 9)
+    val testParsed = Day04.parse(testInput)
+    check(Day04.part1(testParsed) == 18)
+    check(Day04.part2(testParsed) == 9)
 
     val input = readInputAsLines("Day04")
-    val parsed = parse(input)
-    part1(parsed).println()
-    part2(parsed).println()
+    val parsed = Day04.parse(input)
+    Day04.part1(parsed).println()
+    Day04.part2(parsed).println()
 }
 
-private fun parse(input: List<String>) = input.map {
-    it.toCharArray()
-}
+private object Day04 {
+    fun parse(input: List<String>) = input.map {
+        it.toCharArray()
+    }
 
-private fun part1(grid: List<CharArray>): Int {
-    fun search(r: Int, c: Int, mr: Int, mc: Int): Int {
-        var mr = mr
-        var mc = mc
-        var count = 0
-        repeat(4) {
-            var found = buildString {
-                var dr = 0
-                var dc = 0
-                repeat(4) {
-                    append(grid.getOrNull(r + dr)?.getOrNull(c + dc) ?: "")
-                    dr += mr.sign
-                    dc += mc.sign
+    fun part1(grid: List<CharArray>): Int {
+        fun search(r: Int, c: Int, mr: Int, mc: Int): Int {
+            var mr = mr
+            var mc = mc
+            var count = 0
+            repeat(4) {
+                val found = buildString {
+                    var dr = 0
+                    var dc = 0
+                    repeat(4) {
+                        append(grid.getOrNull(r + dr)?.getOrNull(c + dc) ?: "")
+                        dr += mr.sign
+                        dc += mc.sign
+                    }
                 }
+                if (found == "XMAS") {
+                    count++
+                }
+                mr = -mc.also { mc = mr }
             }
-            if (found == "XMAS") {
-                count++
+            return count
+        }
+
+        var total = 0
+        for ((r, row) in grid.withIndex()) {
+            for (c in row.indices) {
+                // search up, down, left, right
+                total += search(r, c, 4, 0)
+                // search diagonal and anti-diagonal
+                total += search(r, c, 4, 4)
             }
-            mr = -mc.also { mc = mr }
         }
-        return count
+        return total
     }
 
-    var total = 0
-    for ((r, row) in grid.withIndex()) {
-        for (c in row.indices) {
-            // search up, down, left, right
-            total += search(r, c, 4, 0)
-            // search diagonal and anti-diagonal
-            total += search(r, c, 4, 4)
+    fun part2(grid: List<CharArray>): Int {
+        fun search(r: Int, c: Int): Int {
+            val diag =
+                (grid.getOrNull(r - 1)?.getOrNull(c - 1) == 'M' && grid.getOrNull(r + 1)?.getOrNull(c + 1) == 'S') ||
+                        (grid.getOrNull(r - 1)?.getOrNull(c - 1) == 'S' && grid.getOrNull(r + 1)
+                            ?.getOrNull(c + 1) == 'M')
+            val anti =
+                (grid.getOrNull(r - 1)?.getOrNull(c + 1) == 'M' && grid.getOrNull(r + 1)?.getOrNull(c - 1) == 'S') ||
+                        (grid.getOrNull(r - 1)?.getOrNull(c + 1) == 'S' && grid.getOrNull(r + 1)
+                            ?.getOrNull(c - 1) == 'M')
+            return if (grid[r][c] == 'A' && diag && anti) 1 else 0
         }
-    }
-    return total
-}
 
-private fun part2(grid: List<CharArray>): Int {
-    fun search(r: Int, c: Int): Int {
-        var diag = (grid.getOrNull(r - 1)?.getOrNull(c - 1) == 'M' && grid.getOrNull(r + 1)?.getOrNull(c + 1) == 'S') ||
-                (grid.getOrNull(r - 1)?.getOrNull(c - 1) == 'S' && grid.getOrNull(r + 1)?.getOrNull(c + 1) == 'M')
-        val anti = (grid.getOrNull(r - 1)?.getOrNull(c + 1) == 'M' && grid.getOrNull(r + 1)?.getOrNull(c - 1) == 'S') ||
-                (grid.getOrNull(r - 1)?.getOrNull(c + 1) == 'S' && grid.getOrNull(r + 1)?.getOrNull(c - 1) == 'M')
-        return if (grid[r][c] == 'A' && diag && anti) 1 else 0
-    }
-
-    var total = 0
-    for ((r, row) in grid.withIndex()) {
-        for (c in row.indices) {
-            total += search(r, c)
+        var total = 0
+        for ((r, row) in grid.withIndex()) {
+            for (c in row.indices) {
+                total += search(r, c)
+            }
         }
+        return total
     }
-    return total
 }
